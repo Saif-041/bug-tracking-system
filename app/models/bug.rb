7477de3969f5f9746799b5e94644ee8c8c92164c
Saif-041 <sp19-bse-041@cuilahore.edu.pk>
2023-04-03 23:35:53 +0500

    checking heroku issue resolve or not

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -1,7 +1,7 @@
 class Bug < ApplicationRecord
     # mount_uploaders :screenshot, AvatarUploader
     mount_uploader :screenshot, AvatarUploader
-
+    # serialize :screenshot, JSON # If you use SQLite, add this line.
 
     validates :title, presence: true, uniqueness: true
     validates :bug_status, presence: true
--- db/migrate/20230329130723_add_avatar_to_bugs.rb
+++ db/migrate/20230329130723_add_avatar_to_bugs.rb
@@ -1,5 +1,5 @@
 class AddAvatarToBugs < ActiveRecord::Migration[7.0]
   def change
-    add_column :bugs, :screenshot, :string
+    add_column :bugs, :screenshot, :json
   end
 end
